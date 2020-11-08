b7b72e2fb52d4c59b69e4106e0ca4c618aa2ead7
Dan Korzh <lienglex@gmail.com>
2020-11-08 20:42:28 +0200

    add: expiryDayValidation, tests

--- src/__tests__/expiryDayValidation.test.js
+++ src/__tests__/expiryDayValidation.test.js
@@ -8,4 +8,44 @@ describe('save error occurred at expiry day validation', () => {
     afterAll(() => {
         sessionStorage.clear()
     })
+
+    it('current day - is today, expiry day - 12/2020 must not save error', () => {
+        expiryDayValidation(2020,
+            12,
+            'expiration-day',
+            'error at expiry day')
+        expect(sessionStorage.getItem('errors')).toStrictEqual("{}")
+    })
+
+    it('current day - is today, expiry day - 11/2020 must save error', () => {
+        expiryDayValidation(2020,
+            11,
+            'expiration-day',
+            'error at expiry day')
+        expect(sessionStorage.getItem('errors')).toStrictEqual("{\"expiration-day\":\"error at expiry day\"}")
+    })
+
+    it('current day - is today, expiry day - 05/2020 must save error', () => {
+        expiryDayValidation(2020,
+            5,
+            'expiration-day',
+            'error at expiry day')
+        expect(sessionStorage.getItem('errors')).toStrictEqual("{\"expiration-day\":\"error at expiry day\"}")
+    })
+
+    it('current day - is today, expiry day - 0/2020 must save error', () => {
+        expiryDayValidation(2020,
+            0,
+            'expiration-day',
+            'error at expiry day')
+        expect(sessionStorage.getItem('errors')).toStrictEqual("{\"expiration-day\":\"error at expiry day\"}")
+    })
+
+    it('current day - is today, expiry day - 13/2020 must save error', () => {
+        expiryDayValidation(2020,
+            13,
+            'expiration-day',
+            'error at expiry day')
+        expect(sessionStorage.getItem('errors')).toStrictEqual("{\"expiration-day\":\"error at expiry day\"}")
+    })
 })
