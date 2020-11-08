2333ea54e1fef7111ea524de4cd7af8ee21529d7
Dan Korzh <lienglex@gmail.com>
2020-11-08 22:52:51 +0200

    add: secondForm

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@ import '../css/index.css'
 
 import inputPhoneMask from './firstForm/inputPhoneMask'
 import firstFormHandler from './firstForm/formHandler'
+
+import secondFormHandler from './secondForm/formHandler'
+import {showAppropriateSubscription} from "./secondForm/showAppropriateSubscription";
+
+secondFormHandler()
+showAppropriateSubscription()
