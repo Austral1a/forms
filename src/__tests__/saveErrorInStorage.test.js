3b3fd98b4beb76541a5e06e0d0bdc88e246f0987
Dan Korzh <lienglex@gmail.com>
2020-11-08 20:44:57 +0200

    add: saveErrorInStorage, tests for first/last name

--- src/__tests__/saveErrorInStorage.test.js
+++ src/__tests__/saveErrorInStorage.test.js
@@ -9,4 +9,31 @@ describe('must save error into session storage occurred at inputs', () => {
         sessionStorage.clear()
     })
 
+    describe('first/last name', () => {
+        const re = /^[a-zA-Z]+$/g
+        it("'Kyl22' must save error into session storage", () => {
+            saveErrorInStorage(re, 'Kyl22', 'first-name', 'error at first name')
+
+            expect(sessionStorage.getItem('errors')).toStrictEqual("{\"first-name\":\"error at first name\"}")
+        })
+
+        it("'Kyle' must not save error into session storage", () => {
+            saveErrorInStorage(re, 'Kyle', 'first-name', 'error at first name')
+
+            expect(sessionStorage.getItem('errors')).toStrictEqual("{}")
+        })
+
+        it("'Br0wn' must save error into session storage", () => {
+            saveErrorInStorage(re, 'Br0wn', 'last-name', 'error at last name')
+
+            expect(sessionStorage.getItem('errors')).toStrictEqual("{\"last-name\":\"error at last name\"}")
+        })
+
+        it("'Br0wn' must save error into session storage", () => {
+            saveErrorInStorage(re, 'Br0wn', 'last-name', 'error at last name')
+
+            expect(sessionStorage.getItem('errors')).toStrictEqual("{\"last-name\":\"error at last name\"}")
+        })
+    })
+
 })
