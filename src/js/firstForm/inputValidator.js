0efbfc15e57b9b2a920c1d749b16cc1d193bde17
Dan Korzh <lienglex@gmail.com>
2020-11-08 22:23:51 +0200

    add: saveErrorInStorage func

--- src/js/firstForm/inputValidator.js
+++ src/js/firstForm/inputValidator.js
@@ -1,3 +1,11 @@
 export const isTextNotPassRe = (validator, inputText) => {
     return !validator.test(inputText)
 }
+
+export const saveErrorInStorage = (validator, inputText, inputName, errorText) => {
+    let errorsObject = JSON.parse(sessionStorage.getItem('errors'))
+    if(isTextNotPassRe(validator, inputText)) {
+        errorsObject[inputName] = errorText
+    }
+    sessionStorage.setItem('errors', JSON.stringify(errorsObject))
+}
