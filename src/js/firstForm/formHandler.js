9343d74bd51a5fc8636620c33875e66499ba0484
Dan Korzh <lienglex@gmail.com>
2020-11-08 22:25:29 +0200

    add: import inputValidator; validationErros object

---
+++ src/js/firstForm/formHandler.js
@@ -0,0 +1,8 @@
+import {inputValidator} from './inputValidator'
+
+const validationErrors = {
+    first_name: 'First name must contain only letters and not to be empty',
+    last_name: 'Last name must contain only letters and not to be empty',
+    email: "Email must be appropriate to 'email@example.com'",
+    phone: "Phone must be appropriate to '(123) 456 - 7890'"
+}
