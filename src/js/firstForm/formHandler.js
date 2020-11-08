17d7ce5de6136351a6ca6447000fc6660d48ec73
Dan Korzh <lienglex@gmail.com>
2020-11-08 22:27:19 +0200

    add: firstForm event, initialize inputs

--- src/js/firstForm/formHandler.js
+++ src/js/firstForm/formHandler.js
@@ -6,3 +6,19 @@ const validationErrors = {
     email: "Email must be appropriate to 'email@example.com'",
     phone: "Phone must be appropriate to '(123) 456 - 7890'"
 }
+
+const firstFormHandler = () => {
+    const firstForm = document.querySelector('#first-form')
+
+    firstForm.addEventListener('submit', (e) => {
+        sessionStorage.setItem('errors', "{}")
+
+        const firstNameVal = document.querySelector('#first-name').value
+        const lastNameVal = document.querySelector('#last-name').value
+        const emailVal = document.querySelector('#email').value
+        const phoneVal = document.querySelector('#phone').value
+
+    })
+}
+
+export default firstFormHandler
