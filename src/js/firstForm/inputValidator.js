23ae39a03d1b998275fb691a1d7456ad8f2e13af
Dan Korzh <lienglex@gmail.com>
2020-11-09 20:23:42 +0200

    rename isTextNotPassRe -> isInputValid

--- src/js/firstForm/inputValidator.js
+++ src/js/firstForm/inputValidator.js
@@ -1,10 +1,10 @@
-export const isTextNotPassRe = (validator, inputText) => {
+export const isInputValid = (validator, inputText) => {
     return !validator.test(inputText)
 }
 
 export const saveErrorInStorage = (validator, inputText, inputName, errorText) => {
     let errorsObject = JSON.parse(sessionStorage.getItem('errors'))
-    if(isTextNotPassRe(validator, inputText)) {
+    if(isInputValid(validator, inputText)) {
         errorsObject[inputName] = errorText
     }
     sessionStorage.setItem('errors', JSON.stringify(errorsObject))
