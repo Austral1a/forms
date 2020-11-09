ffdeb3d7b6c557f1abfd43aa131df3e2f7dbdb1c
Daniil Korzh <lienglex@gmail.com>
2020-11-09 12:49:48 +0200

    moved check on lastFrom into else block

--- src/js/switchToNextForm.js
+++ src/js/switchToNextForm.js
@@ -2,16 +2,16 @@ const switchToNextForm = (fromForm, toForm, formEvent, lastForm) => {
     const errors = Object.values(JSON.parse(sessionStorage.getItem('errors')))
     const errorsContainer = document.querySelector('header')
     // if there are errors it will show it on a screen
-    if(lastForm) {
-        alert('success')
-        return
-    }
     if(errors.length > 0) {
         formEvent.preventDefault()
 
         errorsContainer.innerHTML = errors.join(',  <br>')
         // otherwise, show next form
     } else {
+        if(lastForm) {
+            alert('success')
+            return
+        }
         formEvent.preventDefault()
         // clean up errors
         errorsContainer.innerHTML = ''
@@ -20,7 +20,6 @@ const switchToNextForm = (fromForm, toForm, formEvent, lastForm) => {
 
         // show next form
         toForm.style.display = 'flex'
-
     }
 }
 
