43ec9f77b8b034a91038d281321e0e962542323a
Daniil Korzh <lienglex@gmail.com>
2020-11-10 16:31:16 +0200

    add: Themes object with light and dark mode

---
+++ src/js/themes.js
@@ -0,0 +1,16 @@
+const themes = {
+    light: {
+        bodyBackColor: '#9EB3C2',
+        formBackColor: '#E6EBEF',
+        formElemBackColor: '#fff',
+        textColor: '#000000'
+    },
+    dark: {
+        bodyBackColor: '#222831',
+        formBackColor: '#30475e',
+        formElemBackColor: '#f6f5f1',
+        textColor: '#fff'
+    }
+}
+
+export default themes
