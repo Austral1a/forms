8d86e1c6bd8b88e5c5d85319319df59b640d959b
Daniil Korzh <lienglex@gmail.com>
2020-11-11 13:26:02 +0200

    extracted subsciption object into common folder

--- src/js/thirdForm/showAppropriateSubscription.js
+++ src/js/thirdForm/showAppropriateSubscription.js
@@ -1,16 +1,4 @@
-export const subscriptions = {
-    us: {
-        free: '0$',
-        basic: '20$',
-        premium: '60$'
-    },
-    ua: {
-        free: '0₴',
-        // imagine that price of each plan got from some resource
-        basic: `${20 * 28}₴`,
-        premium: `${60 * 28}₴`
-    }
-}
+import subscriptions from '../common/subscriptions'
 
 export const listener = (country, plan, result) => {
     result.textContent = subscriptions[country.value][plan.value]
