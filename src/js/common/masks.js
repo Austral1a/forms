d16c08dd9a5e86871203a34d4f38b3714539c6f6
Daniil Korzh <lienglex@gmail.com>
2020-11-11 13:06:39 +0200

    add: card number mask

--- src/js/common/masks.js
+++ src/js/common/masks.js
@@ -6,3 +6,12 @@ export const expiryDayMask = () => {
         e.target.value = !x[2] ? x[1] : x[1] + '/' + x[2];
     });
 };
+
+export const cardMask = () => {
+    const cardInputValue = document.querySelector('#card');
+
+    cardInputValue.addEventListener('input', (e) => {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '');
+    });
+};
