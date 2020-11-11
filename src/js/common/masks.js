6aa8e8fb5722c83bf81df3a5d7f34e8f38c16840
Daniil Korzh <lienglex@gmail.com>
2020-11-11 13:09:36 +0200

    add: phoneMask to common masks file

--- src/js/common/masks.js
+++ src/js/common/masks.js
@@ -15,3 +15,12 @@ export const cardMask = () => {
         e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '');
     });
 };
+
+export let phoneMask = () => {
+    const phoneInput = document.querySelector('#phone');
+
+    phoneInput.addEventListener('input', (e) => {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? ' - ' + x[3] : '');
+    });
+};
