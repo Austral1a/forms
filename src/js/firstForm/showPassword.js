9369313c0e13d4cc39d03e385c1a6bbdee1479d2
Daniil Korzh <lienglex@gmail.com>
2020-11-11 10:18:49 +0200

    add: set svg as back-image

--- src/js/firstForm/showPassword.js
+++ src/js/firstForm/showPassword.js
@@ -1,13 +1,23 @@
+import visibility_on from '../../assets/visibility_on.svg'
+import visibility_off from '../../assets/visibility_off.svg'
+
 const showPassword = () => {
     const password = document.querySelector('#password')
     const button = document.querySelector('.form__password-show')
 
+    // set visibility off as default
+    button.style.backgroundImage = `url('${visibility_off}')`
+
     button.addEventListener('mousedown', () => {
         password.setAttribute('type', 'text')
+
+        button.style.backgroundImage = `url('${visibility_on}')`
     })
 
     button.addEventListener('mouseup', () => {
         password.setAttribute('type', 'password')
+
+        button.style.backgroundImage = `url('${visibility_off}')`
     })
 }
 
