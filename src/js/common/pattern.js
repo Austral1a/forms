c143f391f515cc01d77af385f606deee293b9f05
Daniil Korzh <lienglex@gmail.com>
2020-11-11 12:36:24 +0200

    add: common patterns object

---
+++ src/js/common/pattern.js
@@ -0,0 +1,11 @@
+const patterns = {
+    name: /^[a-zA-Z]+$/, // matches first name, last name
+    email: /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/,
+    phone: /^\(\d{3}\)\s\d{3}\s-\s\d{4}$/,
+    username: /\w+/i,
+    password: /\w{6,}/,
+    card: /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/,
+    cvv: /^\d{3}$/
+}
+
+export default patterns
