9977eef4b590ad15520c4427acfea5c4f5d3881f
Daniil Korzh <lienglex@gmail.com>
2020-11-12 13:29:34 +0200

    tests refactor

--- src/__tests__/subscription.test.js
+++ src/__tests__/subscription.test.js
@@ -1,32 +1,32 @@
-import {subscriptions as sub} from '../js/thirdForm/showAppropriateSubscription'
+import {subscriptions as sub} from '../js/common/subscriptions'
 
 describe('subscription must return accurate value', () => {
     describe('country - us', () => {
         const us = 'us'
-        it('us -> free, must return 0$', () => {
+        it('free plan, must return 0$', () => {
             expect(sub[us]['free']).toBe('0$')
         })
 
-        it('us -> basic, must return 20$', () => {
+        it('basic plan, must return 20$', () => {
             expect(sub[us]['basic']).toBe('20$')
         })
 
-        it('us -> premium, must return 60$', () => {
+        it('premium plan, must return 60$', () => {
             expect(sub[us]['premium']).toBe('60$')
         })
     })
 
     describe('country - ua', () => {
         const ua = 'ua'
-        it('us -> free, must return 0₴', () => {
+        it('free plan, must return 0₴', () => {
             expect(sub[ua]['free']).toBe('0₴')
         })
 
-        it('us -> basic, must return 560₴', () => {
+        it('basic plan, must return 560₴', () => {
             expect(sub[ua]['basic']).toBe('560₴')
         })
 
-        it('us -> premium, must return 1680₴', () => {
+        it('premium plan, must return 1680₴', () => {
             expect(sub[ua]['premium']).toBe('1680₴')
         })
 
