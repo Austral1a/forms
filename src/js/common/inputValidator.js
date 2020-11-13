8e4852b7406050837633c5baa42bbc77985a5fb4
Daniil Korzh <lienglex@gmail.com>
2020-11-13 12:23:11 +0200

    deleted useless input

--- src/js/common/inputValidator.js
+++ src/js/common/inputValidator.js
@@ -4,7 +4,6 @@ import isInputValid from './isInputValid';
 // saving errors needs to allow user switch to the next form
 export const saveError = (validator, inputText, inputName, errorText) => {
     const inputError = document.querySelector(`.form__${inputName}-error`);
-    const input = document.querySelector(`#${inputName}`);
     if(!isInputValid(validator, inputText)) {
         errors.set(inputName, errorText);
 
